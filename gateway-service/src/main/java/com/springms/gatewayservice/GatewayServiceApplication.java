1ce0e6f59fee3235d87010b42b23cbf58bdf0345
sujan-sen <sujankrsen@gmail.com>
2020-04-07 22:04:38 +0530

    Added security on gateway

--- gateway-service/src/main/java/com/springms/gatewayservice/GatewayServiceApplication.java
+++ gateway-service/src/main/java/com/springms/gatewayservice/GatewayServiceApplication.java
@@ -4,7 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
+@EnableAuthorizationServer
 @EnableEurekaClient
 //Overriding default Ribbon algorithm from Zuul
 //@RibbonClients(defaultConfiguration = RibbonConfiguration.class)
